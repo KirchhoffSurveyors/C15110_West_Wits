a446fe4929a3b93be1dc12749c812ef74ca9bcf2
David Barkhuisen <dave@kirchhoffsurveyors.co.za>
2018-02-01 14:07:01 +0200

    Add files via upload

---
+++ data/WestWitsPAIAPointsWG29_2.js
@@ -0,0 +1,25 @@
+var json_WestWitsPAIAPointsWG29_2 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Point": "A", "X": 112043.71, "Y": 2899698.06 }, "geometry": { "type": "Point", "coordinates": [ 27.879000028159467, -26.202000008398421 ] } },
+{ "type": "Feature", "properties": { "Point": "B", "X": 108845.36, "Y": 2900825.72 }, "geometry": { "type": "Point", "coordinates": [ 27.910900199151115, -26.21242222174806 ] } },
+{ "type": "Feature", "properties": { "Point": "C", "X": 108881.57, "Y": 2901080.72 }, "geometry": { "type": "Point", "coordinates": [ 27.910516506077887, -26.214720684787611 ] } },
+{ "type": "Feature", "properties": { "Point": "D", "X": 109201.63, "Y": 2902106.72 }, "geometry": { "type": "Point", "coordinates": [ 27.907227902433029, -26.223955343423714 ] } },
+{ "type": "Feature", "properties": { "Point": "E", "X": 111079.34, "Y": 2909037.33 }, "geometry": { "type": "Point", "coordinates": [ 27.887845103156295, -26.286354657209603 ] } },
+{ "type": "Feature", "properties": { "Point": "F", "X": 118710.58, "Y": 2905684.89 }, "geometry": { "type": "Point", "coordinates": [ 27.811759993448369, -26.255489988645909 ] } },
+{ "type": "Feature", "properties": { "Point": "G", "X": 115835.93, "Y": 2906122.04 }, "geometry": { "type": "Point", "coordinates": [ 27.840490011569155, -26.259670003197467 ] } },
+{ "type": "Feature", "properties": { "Point": "H", "X": 115854.21, "Y": 2905307.72 }, "geometry": { "type": "Point", "coordinates": [ 27.840380040361385, -26.252320043903975 ] } },
+{ "type": "Feature", "properties": { "Point": "J", "X": 118815.72, "Y": 2905334.57 }, "geometry": { "type": "Point", "coordinates": [ 27.810739965166196, -26.252319999127106 ] } },
+{ "type": "Feature", "properties": { "Point": "K", "X": 118863.67, "Y": 2905009.21 }, "geometry": { "type": "Point", "coordinates": [ 27.810290011230105, -26.249379982782695 ] } },
+{ "type": "Feature", "properties": { "Point": "L", "X": 118980.96, "Y": 2904533.78 }, "geometry": { "type": "Point", "coordinates": [ 27.809159993097602, -26.245079984964381 ] } },
+{ "type": "Feature", "properties": { "Point": "M", "X": 113826.77, "Y": 2904512.92 }, "geometry": { "type": "Point", "coordinates": [ 27.860740023117796, -26.245310032787749 ] } },
+{ "type": "Feature", "properties": { "Point": "N", "X": 113736.56, "Y": 2907044.22 }, "geometry": { "type": "Point", "coordinates": [ 27.861420045630133, -26.268159989118665 ] } },
+{ "type": "Feature", "properties": { "Point": "P", "X": 112188.95, "Y": 2900473.89 }, "geometry": { "type": "Point", "coordinates": [ 27.877479968124508, -26.208989962446363 ] } },
+{ "type": "Feature", "properties": { "Point": "A", "X": 116606.32, "Y": 2903346.4 }, "geometry": { "type": "Point", "coordinates": [ 27.833029988557161, -26.234560001833756 ] } },
+{ "type": "Feature", "properties": { "Point": "B", "X": 116611.65, "Y": 2904085.58 }, "geometry": { "type": "Point", "coordinates": [ 27.832910040039049, -26.241229981168193 ] } },
+{ "type": "Feature", "properties": { "Point": "C", "X": 117730.7, "Y": 2904109.01 }, "geometry": { "type": "Point", "coordinates": [ 27.821709951606849, -26.241350014894632 ] } },
+{ "type": "Feature", "properties": { "Point": "D", "X": 117552.91, "Y": 2903768.3 }, "geometry": { "type": "Point", "coordinates": [ 27.823519992032793, -26.23829001461959 ] } },
+{ "type": "Feature", "properties": { "Point": "E", "X": 117701.97, "Y": 2903531.4 }, "geometry": { "type": "Point", "coordinates": [ 27.822049986844974, -26.236140000033544 ] } }
+]
+}
